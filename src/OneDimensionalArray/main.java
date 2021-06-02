3155389202c3ed60ca0376991e7185ddff945c04
dlghrms1992 <dlghrm1992@naver.com>
2021-06-02 22:57:17 +0900

    Hello BackJoon Algorithm !!

---
+++ src/OneDimensionalArray/main.java
@@ -0,0 +1,27 @@
+package OneDimensionalArray;
+
+import java.util.Scanner;
+
+public class main {
+
+	private Scanner sc;
+		
+	public static void main(String[] args) {
+	
+	}
+	
+	//나머지
+	public void Remainder() {
+		sc = new Scanner(System.in);
+		//1. 숫자 열개를 입력 받는다. 이를 42로 나눈 나머지를 구한다.
+		int[] arr = new int[10];
+		int b = sc.nextInt();
+		for(int a : arr){
+			int result = a%b;
+			
+		}
+		
+		
+	}
+
+}
