02da79a64d7bbf1570dec3613dd04f3c4f696530
dlghrms1992 <dlghrms1992@naver.com>
2021-06-09 22:18:29 +0900

    10809, 11720 success~!

--- src/StringAlghorithm/Main.java
+++ src/StringAlghorithm/Main.java
@@ -3,15 +3,57 @@ package StringAlghorithm;
 import java.util.Scanner;
 
 class Main {
-
+	
+	private static Scanner sc;
+	
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(ASCIIConversion(scanner.next()));
+//		System.out.println(ASCIIConversion(scanner.next()));
+//		System.out.println(NumberWithoutSpace());
+		FindAlpahbet();
 		
 	}
 
-	 public static int ASCIIConversion(String text) {
+	private static void FindAlpahbet() {
+		
+		sc = new Scanner(System.in);
+		int[] arr = new int[26];
+		String s = sc.nextLine();
+
+		for(char a = 'a' ; a <= 'z'  ; a++) {
+			System.out.print(s.indexOf(a) + "");
+		}
+//		for(int i = 0 ; i < arr.length ; i++) {
+//			arr[i] = -1;
+//		}
+//
+//		for(int i = 0 ; i < s.length() ; i ++) {
+//			char ch = s.charAt(i);
+//			arr[ch - 'a'] = i;
+//		}
+//
+//		for(int value : arr) {
+//			System.out.print(value + " ");
+//		}
+	}
+
+	private static int NumberWithoutSpace() {
+		
+		 sc = new Scanner(System.in);
+		 int N = sc.nextInt();
+		 int[] arr = new int[N];
+		 String text = sc.next();
+		 sc.close();
+		 int sum = 0;
+		 
+		 for(int i = 0 ; i < N ; i++) {
+			 sum += text.charAt(i)-'0';
+		 }
+		 
+		return sum;
+	}
+
+	public static int ASCIIConversion(String text) {
 	
 		int conversionNumber = text.charAt(0);
 		
