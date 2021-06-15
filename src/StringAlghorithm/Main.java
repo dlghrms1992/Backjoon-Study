c1fb8fcdf5b673324717ddb635348a9da8c4862c
HGLEE <HGLEE@DESKTOP-LO6JA8B>
2021-06-15 21:51:11 +0900

    2908 Success!

--- src/StringAlghorithm/Main.java
+++ src/StringAlghorithm/Main.java
@@ -1,5 +1,8 @@
 package StringAlghorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -14,7 +17,26 @@ class Main {
 //		FindAlpahbet();
 //		StringRepeat();
 //		EnWord();
-		numberOfwords();
+//		numberOfwords();
+		try {
+			speakConstant();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void speakConstant() throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		System.out.println(A > B ? A : B);
 		
 	}
 
