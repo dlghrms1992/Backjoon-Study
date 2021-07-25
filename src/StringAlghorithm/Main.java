37ebbcd78aa6b74982ac3e4a539ae9789e23f18a
EMG <dlghrms1992@naver.com>
2021-07-25 13:46:57 +0900

    1193 success~!

--- src/StringAlghorithm/Main.java
+++ src/StringAlghorithm/Main.java
@@ -45,8 +45,32 @@ class Main {
 	}
 
 	private static void FindAFountain() {
-		// TODO Auto-generated method stub
 		
+		Scanner in = new Scanner(System.in);
+		int X = in.nextInt();
+		
+		int cross_count = 1, prev_count_sum = 0;
+		
+		while(true) {
+
+			// 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+			if(X <= prev_count_sum + cross_count) {
+				// 분모가 큰 수부터 시작
+				// 분모는 대각선 개수 - (X 번째 - 직전 대각선까지의 누적합 - 1) 
+				// 분자는 X 번째 - 직전 대각선까지의 누적합 
+				if(cross_count % 2 == 1) {
+					System.out.println((cross_count - (X - prev_count_sum -1)) + "/" + (X - prev_count_sum));
+					break;
+				}	else {	// 대각선의 개수가 짝수라면 
+					// 홀수일 때의 출력을 반대로 
+					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+					break;
+				}
+			} else {
+				prev_count_sum += cross_count;
+				cross_count++;
+			}
+		}
 	}
 
 	private static void Honeycomb() {
